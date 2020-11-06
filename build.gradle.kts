4177efba33222775843e242fab42a1b38b10a2e0
ht-chchoi <chchoi@hdtel.co.kr>
2020-11-06 09:15:35 +0900

    jdk 11로 변경

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,14 +3,14 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.springframework.boot") version "2.3.4.RELEASE"
     id("io.spring.dependency-management") version "1.0.10.RELEASE"
-    kotlin("jvm") version "1.3.72"
-    kotlin("plugin.spring") version "1.3.72"
-    kotlin("plugin.jpa") version "1.3.72"
+    kotlin("jvm") version "1.4.10"
+    kotlin("plugin.spring") version "1.4.10"
+    kotlin("plugin.jpa") version "1.4.10"
 }
 
 group = "com.friendship41"
 version = "0.0.1"
-java.sourceCompatibility = JavaVersion.VERSION_1_8
+java.sourceCompatibility = JavaVersion.VERSION_11
 
 configurations {
     compileOnly {
@@ -33,8 +33,6 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
-    // https://mvnrepository.com/artifact/org.apache.httpcomponents/httpclient
-    implementation("org.apache.httpcomponents:httpclient:4.5.13")
 
     // https://mvnrepository.com/artifact/mysql/mysql-connector-java
     implementation("mysql:mysql-connector-java:8.0.22")
@@ -42,9 +40,6 @@ dependencies {
     implementation("com.zaxxer:HikariCP:3.4.5")
     // https://mvnrepository.com/artifact/org.bgee.log4jdbc-log4j2/log4jdbc-log4j2-jdbc4
     implementation("org.bgee.log4jdbc-log4j2:log4jdbc-log4j2-jdbc4:1.16")
-
-    compileOnly("org.projectlombok:lombok")
-    annotationProcessor("org.projectlombok:lombok")
 
 
     testImplementation("org.springframework.boot:spring-boot-starter-test") {
@@ -59,6 +54,6 @@ tasks.withType<Test> {
 tasks.withType<KotlinCompile> {
     kotlinOptions {
         freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = "1.8"
+        jvmTarget = "11"
     }
 }
--- src/main/kotlin/com/friendship41/memberserver/config/ResourceServerConfig.kt
+++ src/main/kotlin/com/friendship41/memberserver/config/ResourceServerConfig.kt
@@ -1,21 +1,14 @@
 package com.friendship41.memberserver.config
 
-import org.apache.http.conn.ssl.NoopHostnameVerifier
-import org.apache.http.conn.ssl.SSLConnectionSocketFactory
-import org.apache.http.impl.client.HttpClients
-import org.apache.http.ssl.TrustStrategy
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Primary
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter
 import org.springframework.security.oauth2.provider.OAuth2Authentication
 import org.springframework.security.oauth2.provider.token.DefaultAccessTokenConverter
 import org.springframework.security.oauth2.provider.token.RemoteTokenServices
-import org.springframework.web.client.RestTemplate
-import java.security.cert.X509Certificate
 
 @Configuration
 @EnableResourceServer
@@ -35,25 +28,12 @@ class MemberResourceServerConfig: ResourceServerConfigurerAdapter() {
     @Bean
     fun tokenService(): RemoteTokenServices {
         val tokenService = RemoteTokenServices()
-        tokenService.setRestTemplate(restTemplate())
         tokenService.setCheckTokenEndpointUrl("https://192.168.0.200:42222/oauth/check_token")
         tokenService.setClientId("member_server")
         tokenService.setClientSecret("thisismember")
         tokenService.setAccessTokenConverter(CustomAccessTokenConverter())
         return tokenService
     }
-}
-
-@Bean
-fun restTemplate(): RestTemplate {
-    val acceptingTrustStrategy = TrustStrategy{ _: Array<X509Certificate>, _: String -> true
-    }
-    val sslContext = org.apache.http.ssl.SSLContexts.custom().loadTrustMaterial(null, acceptingTrustStrategy).build()
-    val csf = SSLConnectionSocketFactory(sslContext, NoopHostnameVerifier())
-    val httpClient = HttpClients.custom().setSSLSocketFactory(csf).build()
-    val requestFactory = HttpComponentsClientHttpRequestFactory()
-    requestFactory.httpClient = httpClient
-    return RestTemplate(requestFactory)
 }
 
 class CustomAccessTokenConverter: DefaultAccessTokenConverter() {
